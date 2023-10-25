cd0a0372dd17813869d9000c3c27a4e4ae3832c9
Atte Harrikari <atte.harrikari@ramboll.fi>
2023-10-25 14:03:23 +0300

    Add project files.

---
+++ GatekeeperComponent.cs
@@ -0,0 +1,91 @@
+using System;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+namespace Gatekeeper
+{
+    public class GH_GatekeeperComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GH_DataDamComponent class.
+        /// </summary>
+        public GH_GatekeeperComponent()
+          : base("Gatekeeper", "GK",
+              "The Gatekeeper component is a powerful tool designed to manage the flow of data within Grasshopper for Rhino. " +
+              "It acts as a conditional gate that can prevent data from propagating further in a Grasshopper definition based on a boolean condition, " +
+              "without triggering a recomputation of the solution. " +
+              "This component is particularly useful when you need to control the data flow based on specific conditions, " +
+              "and it ensures a seamless user experience by retaining the previous data state.",
+              "Params", "Util")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Data", "D", "Data", GH_ParamAccess.tree);
+            pManager.AddBooleanParameter("Pass", "P", "True, if data is passed forward", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Data", "D", "Data", GH_ParamAccess.item);
+        }
+
+        private bool _pass = false;
+        private GH_Structure<IGH_Goo> _data = new GH_Structure<IGH_Goo>();
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            bool compute = false;
+            DA.GetData(1, ref compute);
+
+            if (compute)
+            {
+                DA.GetDataTree(0, out GH_Structure<IGH_Goo> dataTree);
+
+                if (!_pass)
+                    _data = dataTree.ShallowDuplicate();
+
+                DA.SetDataTree(0, dataTree);
+
+                foreach (IGH_Param recipient in Params.Output[0].Recipients)
+                    recipient.ExpireSolution(recompute: true);
+            }
+            else
+            {
+                DA.SetDataTree(0, _data);
+            }
+
+            _pass = compute;
+        }
+
+        protected override void ExpireDownStreamObjects()
+        {
+        }
+
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Gatekeeper.Properties.Resources.GH_Gatekeeper.ToBitmap();
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("F798609B-3A6A-4736-BD18-E59BF1F95D65"); }
+        }
+    }
+}
---
+++ GatekeeperInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace Gatekeeper
+{
+    public class GatekeeperInfo : GH_AssemblyInfo
+    {
+        public override string Name => "Gatekeeper";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("1212c74b-9967-4d3b-92b7-48896b76b6e0");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "Atte Harrikari";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "atte.harrikari@gmail.com";
+    }
+}
